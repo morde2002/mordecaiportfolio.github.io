ff29203ef43cb08aa3a5606f0798c2015b8df2bd
Mordecai Junior Mathenge <mathengemordecai@gmail.com>
2024-02-04 15:28:30 +0300

    hkk

--- form-handler.php
+++ form-handler.php
@@ -7,42 +7,44 @@ require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $subject = $_POST["subject"];
-    $message = $_POST["message"];
-    
+    // Common fields
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    // Instantiate PHPMailer
     $mail = new PHPMailer;
     $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
+    $mail->Host = 'smtp.titan.email';
     $mail->SMTPAuth = true;
-    $mail->Username = 'mathengemordecai@gmail.com';
-    $mail->Password = 'mordecai2002';
+    $mail->Username = 'mordecaiportfolio@xeleratedtech.com';
+    $mail->Password = 'mordecai2002.';
     $mail->SMTPSecure = 'tls';
     $mail->Port = 587;
     $mail->Timeout = 10;
-    
-    $mail->setFrom($email);
-    $mail->addAddress('mathengemordecai@gmail.com', 'Recipient Name');
+
+    // Set common email parameters
+    $mail->setFrom('mordecaiportfolio@xeleratedtech.com', $name);
+    $mail->addAddress('mordecaiportfolio@xeleratedtech.com', 'Recipient Name');
     $mail->isHTML(true);
-    $mail->Subject = 'New Contact Form Submission';
-    
-    
+    $mail->Subject = 'Contact Form Submission';
+
+    // Customize email content
     $mail->Body = "
-        <h1>Contact Details:</h1>
+        <h1>Contact Form Submission</h1>
         <p><strong>Name:</strong> $name</p>
         <p><strong>Email:</strong> $email</p>
-        <p><strong>Subject:</strong> $subject</p>
+        <p><strong>Subject :</strong> $subject</p>
+        <br>
         
-        <h1>Message:</h1>
-        <p>$message</p>
-        
+        <h2><strong>Message :</strong><br> $message</h2>
     ";
-    
+
     // Send email
     if ($mail->send()) {
-        echo '<h1>Your message has been sent successfully. Thank you!</h1>';
-        echo '<a href="index.html">Go Back</a>';
+        echo '<h2>Your message has been sent successfully</h2>';
+        echo '<a href="index.html" class="btn btn-primary">Go Back</a>';
         echo '<script>
                 setTimeout(function() {
                     window.location.href = "index.html";
@@ -52,5 +54,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo 'Message could not be sent.';
         echo 'Mailer Error: ' . $mail->ErrorInfo;
     }
-} 
+}
 ?>
